47c92801734581f8ef85f6dba367e19b811a5e4d
LANGESH KUMAR <langeshkumarbalamurugan@gmail.com>
2025-06-19 14:45:53 +0530

    Time: 2 ms (15.83%), Space: 42.3 MB (37.24%) - LeetHub

---
+++ 1542-consecutive-characters/1542-consecutive-characters.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxPower(String s) {
+        int max=0;
+        int l=0;
+        int r=0;
+        while(r<s.length()){
+            while(r<s.length() && s.charAt(r) == s.charAt(l)) r++;
+            max = Math.max(max, r - l);
+            l = r;
+        }
+        return max;
+    }
+}
